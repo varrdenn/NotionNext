27e7c7feeada1fd24b6d3e107f340c4ec564cb31
ZoBo <89306424+varrdenn@users.noreply.github.com>
2026-05-11 13:23:39 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -36,20 +36,34 @@ module.exports = {
   MUSIC_PLAYER_ORDER: process.env.NEXT_PUBLIC_MUSIC_PLAYER_ORDER || 'list', // 默认播放方式，顺序 list，随机 random
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
-    {
-      name: '风を共に舞う気持ち',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
+  	{
+        name: '告诉我(Live)',
+        artist: '陈绮贞',
+        url: 'https://music.163.com/song/media/outer/url?id=37240058.mp3',
+        cover:
+          'https://p1.music.126.net/haTIbpkwpBOz-pW2WGt37w==/3309530001047798.jpg?param=90y90'
+  	},
+  	{
+      name: '告诉你(Live)',
+      artist: '陈绮贞',
+      url: 'https://music.163.com/song/media/outer/url?id=37239070.mp3',
       cover:
-        'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
-    },
-    {
-      name: '王都グランセル',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
+  'https://p1.music.126.net/haTIbpkwpBOz-pW2WGt37w==/3309530001047798.jpg?param=90y90'
+  	},	
+      {
+        name: '天后',
+        artist: '卢卢快闭嘴',
+        url:  'https://music.163.com/song/media/outer/url?id=2101067848.mp3',
+        cover:
+  'https://p1.music.126.net/_TV32cQnDUP2SAtCPUEtoA==/109951169066704919.jpg?param=90y90'
+  	},	
+      {
+      name: '可你听见了',
+      artist: 'DOUDOU',
+      url: 'https://music.163.com/song/media/outer/url?id=2605274338.mp3',
       cover:
-        'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
-    }
+  'https://p1.music.126.net/7DO7fn5_G7CtSPrTtenvnw==/109951169759746430.jpg?param=90y90'
+  	}
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER:
